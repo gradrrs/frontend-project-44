b6207ee7b8f12c400c964a64427b86b53aea4636
root <root@LAPTOP-C1EJGJ0V>
2024-02-15 14:23:51 +0300

    7

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
-import { progressionNumber } from '../src/src/games/brain-progression.js';
-import { calculate } from '../src/src/games/brain-calc.js';
-import { isEven } from '../src/src/games/brain-even.js';
-import { nod } from '../src/src/games/brain-gcd.js';
-import {cli, importName} from '../src/src/games/cli.js';
-import {isPrimeNumber, isPrime} from '../src/src/games/brain-primenumb.js';
+import cli from "cli.js";
 cli();
