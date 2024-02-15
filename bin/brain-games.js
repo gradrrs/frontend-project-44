254701aee24e141d34fa032c69cddf84e91f0cbe
artem gorbachev <artemlal1998@gmail.com>
2024-02-15 13:57:28 +0300

    6

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import { progressionNumber } from '../src/games/brain-progression.js';
-import { calculate } from '../src/games/brain-calc.js';
-import { isEven } from '../src/games/brain-even.js';
-import { nod } from '../src/games/brain-gcd.js';
-import {cli, importName} from '../src/games/cli.js';
-import {isPrimeNumber, isPrime} from '../src/games/brain-primenumb.js';
+import { progressionNumber } from '../src/src/games/brain-progression.js';
+import { calculate } from '../src/src/games/brain-calc.js';
+import { isEven } from '../src/src/games/brain-even.js';
+import { nod } from '../src/src/games/brain-gcd.js';
+import {cli, importName} from '../src/src/games/cli.js';
+import {isPrimeNumber, isPrime} from '../src/src/games/brain-primenumb.js';
 cli();
