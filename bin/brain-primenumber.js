0aabac4fcdcfb216aef89a8e530dd8625f2334b6
artem gorbachev <artemlal1998@gmail.com>
2024-02-16 15:14:19 +0300

    18

--- bin/brain-primenumber.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { cli } from "../src/src/games/cli.js";
-import isPrimeNumber  from "../src/src/games/brain-primenumber.js";
-cli();
-isPrimeNumber();
--- bin/calc-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import calculate from "../src/src/games/brain-calc.js";
-import { cli } from "../src/src/games/cli.js";
-cli();
-calculate();
