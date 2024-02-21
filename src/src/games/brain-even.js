932fd7107b9d8e9c6815649f45ab6b1ad460d2ff
artem gorbachev <artemlal1998@gmail.com>
2024-02-21 11:51:42 +0300

    52

--- src/src/games/brain-even.js
+++ src/src/games/brain-even.js
@@ -1,36 +1,32 @@
 import {
-  askQuestion, isCorrect, congratulation, questionGame, tryAgain,
-  evenQuest, welcome, brainFalseAnswer,
+  askQuestion, isCorrect, congratulation, questionGame, tryAgain, evenQuest,
 } from '../index.js';
-// import { cli } from './cli.js';
-// import { importName } from '../../../bin/cli.js';
+import { cli } from './cli.js';
+//import { importName } from '../../../bin/cli.js';
 
 const question = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = () => {
-  const massive = {
-    yes: 'no',
-    no: 'yes',
-  };
-  welcome();
+  cli();
   let count = 0;
   questionGame(question);
   for (let i = 0; i < 3; i += 1) {
     const x = String(Math.random())[2];
     evenQuest(x);
+    console.log(x)
     const input = askQuestion();
-    const y = parseInt(x, 10);
-    if ((y % 2 === 0 && input === 'yes') || (y % 2 !== 0 && input === 'no')) {
+    if ((x % 2 === 0 && input === 'yes') || (x % 2 !== 0 && input === 'no')) {
       isCorrect();
       count += 1;
     } else {
-      brainFalseAnswer(input, massive[input]);
+      
       tryAgain();
       break;
     }
   }
-  if (count === 3) {
-    congratulation();
-  }
+    if (count === 3) {
+      congratulation();
+    }
+  
 };
 export default isEven;
